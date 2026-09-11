80dafa7b8421b746b7c582b147607564efe0f70e
raserrano <betoseru23@gmail.com>
2026-09-11 14:48:25 -0600

    fix(filename): adjust filename with 0s to have same length

--- index.js
+++ index.js
@@ -3,32 +3,27 @@ const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
 
-const poasCha = {
-  name: 'poasCha',
-  url: 'http://www.ovsicori.una.ac.cr/images/stories/camaras/livechahuites/camara.jpg'
-}
-const irazu = {
-  name: 'irazu',
-  url: 'http://www.ovsicori.una.ac.cr/images/stories/camaras/liveirazu/camara.jpg'
-}
-const poasSO = {
-  name: 'poasSO',
-  url: 'http://www.ovsicori.una.ac.cr/images/stories/camaras/livepoas/camara.jpg'
-}
-const turrialba = {
-  name: 'turrialba',
-  url: 'http://www.ovsicori.una.ac.cr/images/stories/camaras/liveturrialba/camara.jpg'
-}
-const poas = {
-  name: 'poas',
-  url: 'http://www.ovsicori.una.ac.cr/images/stories/camaras/livecraterpoas/camara.jpg'
-}
-const rincon = {
-  name: 'rincon',
-  url: 'http://www.ovsicori.una.ac.cr/images/stories/camaras/liverincon/camara.jpg'
+const volcanos = {
+  poasCha: {
+    key: 'livechahuites'
+  },
+  irazu: {
+    key: 'liveirazu'
+  },
+  poasSO:{
+    key: 'livepoas'
+  },
+  turrialba:{
+    key: 'liveturrialba'
+  },
+  poas: {
+    key: 'livecraterpoas'
+  },
+  rincon: {
+    key: 'liverincon'
+  }
 }
 
-const volcanos = [poas, poasCha, poasSO, irazu, turrialba, rincon]
 
 const download = function (uri, filename, callback) {
   axios({
@@ -42,16 +37,24 @@ const download = function (uri, filename, callback) {
 
 function grabImages () {
   const when = new Date()
-  const format = `${when.getFullYear()}${when.getMonth() + 1}${when.getDate()}${when.getHours()}${when.getMinutes()}`
+  const parts = [
+    when.getMonth() + 1,
+    when.getDate(),
+    when.getHours(),
+    when.getMinutes()
+  ]
+  const format = `${when.getFullYear()}${parts.map(e => String(e).padStart(2, '0')).join('')}`
   console.log(`Grabbing images at ${when}`)
-  const imagesPath = path.join(__dirname, './images/');
-  volcanos.forEach(v => {
-    download(v.url, `${imagesPath}${v.name}-${format}.jpg`, () => {
-      console.log('done')
+  const imagesPath = path.join(__dirname, './images/')
+
+  Object.entries(volcanos).forEach(([key, value])=> {
+    const url = `http://www.ovsicori.una.ac.cr/images/stories/camaras/${value.key}/camara.jpg`
+    download(url, `${imagesPath}${key}-${format}.jpg`, () => {
+      console.log(`Done downloading ${key}-${format} picture.`)
     })
   })
 }
 
-cron.schedule(`*/${process.env.FREQ} * * * *`, () => {
+cron.schedule(`*/${process.env.FREQ ? process.env.FREQ : 5} * * * *`, () => {
   grabImages()
 })
